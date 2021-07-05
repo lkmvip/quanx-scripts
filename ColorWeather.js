9f92b5056b2a82107ac15f3907f6670216f5c386
Cubarco <marco.l.here@gmail.com>
2021-07-05 16:44:36 +0800

    Update ColorWeather.js

--- ColorWeather.js
+++ ColorWeather.js
@@ -4,8 +4,7 @@ obj['result']['xy_vip_expire'] = 4096483190;
 obj['result']['is_vip'] = true;
 obj['result']['vip_expired_at'] = 4096483190;
 obj['result']['is_xy_vip'] = true;
-obj['result']['wt']['vip']['enabled'] = true;
-obj['result']['wt']['vip']['expired_at'] = 4096483190;
+obj['result']['vip_type'] = "s";
 body = JSON['stringify'](obj);
 $done({
     body
