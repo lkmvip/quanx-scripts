9e68bf7a36d299a22b9d2514cdd3d1e969459f7d
lkmvip <12974141+lkmvip@users.noreply.github.com>
2021-07-07 22:50:49 +0800

    Update ColorWeather.js

--- ColorWeather.js
+++ ColorWeather.js
@@ -4,14 +4,14 @@ obj['result']['is_vip'] = true;
 obj['result']['svip_expired_at'] = 1765512732;
 obj['result']['vip_type'] = "s";
 obj['result']['xy_vip_expire'] = 0;
-obj['wt']['ranking_above'] = 100;
-obj['wt']['vip']['is_auto_renewal'] = false;
-obj['wt']['vip']['enabled'] = true,
-obj['wt']['vip']['svip_expired_at'] = 1765512732;
-obj['wt']['svip_given'] = 365;
-obj['wt']['is_login'] = true;
-obj['wt']['last_acted_at'] = 1725500509;
-obj['wt']['created_at'] = 1594707500;
+obj['result']['wt']['ranking_above'] = 100;
+obj['result']['wt']['vip']['is_auto_renewal'] = false;
+obj['result']['wt']['vip']['enabled'] = true,
+obj['result']['wt']['vip']['svip_expired_at'] = 1765512732;
+obj['result']['wt']['svip_given'] = 365;
+obj['result']['wt']['is_login'] = true;
+obj['result']['wt']['last_acted_at'] = 1725500509;
+obj['result']['wt']['created_at'] = 1594707500;
 body = JSON['stringify'](obj);
 $done({
     body
