d33b43c20fcc2f203fa483265dba13859abf29fc
lkmvip <12974141+lkmvip@users.noreply.github.com>
2021-07-07 22:23:19 +0800

    Update ColorWeather.js

--- ColorWeather.js
+++ ColorWeather.js
@@ -1,7 +1,6 @@
 var body = $response['body'];
 var obj = JSON['parse'](body);
 obj['result']['is_vip'] = true;
-obj['result']['is_vip'] = true;
 obj['result']['svip_expired_at'] = 1765512732;
 obj['result']['xy_vip_expire'] = 0;
 body = JSON['stringify'](obj);
