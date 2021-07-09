593697a5b0b72abd4775ef6f2a4ca14ad0243fda
lkmvip <12974141+lkmvip@users.noreply.github.com>
2021-07-09 23:34:56 +0800

    Update ColorWeather.js

--- ColorWeather.js
+++ ColorWeather.js
@@ -1,13 +1,13 @@
 var body = $response['body'];
 var obj = JSON['parse'](body);
 obj['result']['is_vip'] = true;
-obj['result']['svip_expired_at'] = 1765512732;
+obj['result']['svip_expired_at'] = 1665123505.0679941177;
 obj['result']['vip_type'] = "s";
 obj['result']['xy_vip_expire'] = 0;
 obj['result']['wt']['ranking_above'] = 100;
 obj['result']['wt']['vip']['is_auto_renewal'] = false;
 obj['result']['wt']['vip']['enabled'] = true,
-obj['result']['wt']['vip']['svip_expired_at'] = 1765512732;
+obj['result']['wt']['vip']['svip_expired_at'] = 1665123505.0679941177;
 obj['result']['wt']['svip_given'] = 365;
 obj['result']['wt']['is_login'] = true;
 obj['result']['wt']['last_acted_at'] = 1725500509;
